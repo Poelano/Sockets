16ac810046a206951bd95515d1fd82e5599b3144
Poelano <poelanoserame@gmail.com>
2019-10-08 12:23:26 +0200

    Server defined listening on Port

---
+++ Server.java
@@ -0,0 +1,59 @@
+
+// The skeleton program for Server
+import java.net.*;
+import java.io.*;
+
+public class Server
+{
+	private Socket socket = null;
+	private ServerSocket server = null;
+	private DataInputStream in = null;
+
+	public Server(int port)
+	{
+		try
+		{
+			server = new ServerSocket(port);
+			System.out.println("Server Starteed");	
+
+			System.out.println("Waiting for client to connect...");
+
+			socket = server.accept();
+			System.out.println("Client connection accepted");
+
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			String line = "";
+
+			While(!line.equals("Done"));
+			{
+				try
+				{
+					line = in.readUTF();
+					System.out.printlin(line);
+				}
+				catch (IOException i)
+				{
+					System.out.println(i);
+
+				}
+			}
+
+			socket.close();
+			in.close();
+
+
+		}
+		catch (IOException i)
+		{
+			System.out.println(i);
+		}
+
+
+	}
+}
+
+    public static void main(String args[])
+    {
+        Server server = new Server(5000);
+    }
+}
